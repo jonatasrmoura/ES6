4f2cf51564e641029b469a7224091ee3fb43227a
Jonatas Rosa Moura <jonatas.rmoura@outlook.com>
2020-07-28 15:11:47 -0300

    Arrow Functions
    Praticando vetores em JS EM6

--- bundle.js
+++ bundle.js
@@ -1,22 +1,45 @@
 "use strict";
 
+// ============= Operadores em Array =================
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // cada item do vetor arr, vai passar por essa função
+
+var newArr = arr.map(function (item, index) {
+  return item * index;
+});
+console.log(newArr);
+var sum = arr.reduce(function (total, next) {
+  return total + next;
+});
+console.log(sum); // --------------------------
+
+var filter = arr.filter(function (item) {
+  return item % 2 === 0;
+});
+console.log(filter); // --------------------------
+
+var find = arr.find(function (item) {
+  return item === 4;
+});
+console.log(find);
+/* 
 // ========= variáveis de Escopos ====================================
 function teste(x) {
-  var y = 2;
+    let y = 2;
 
-  if (x > 5) {
-    var _y = 4;
-    console.log(x, _y);
-  }
+    if(x > 5) {
+        let y = 4;
+        console.log(x, y);
+    }
 }
 
-teste(10); // ============= COSNTANTES ===========
+teste(10);
 
-var usuario = {
-  nome: 'João'
-};
+// ============= COSNTANTES ===========
+const usuario = {nome: 'João'};
 usuario.nome = 'Diego';
-console.log(usuario); // ============== CLASSES ==============================================
+console.log(usuario);
+ */
+// ============== CLASSES ==============================================
 
 /* Ele já vai mostrar a soma porque o método é static ele não vai precisar
 definir uma nova classe Matematica e não depende de mais nenhuma informação
--- main.js
+++ main.js
@@ -1,3 +1,54 @@
+
+// ============= Operadores em Array =================
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// cada item do vetor arr, vai passar por essa função
+const newArr = arr.map(function(item, index) {
+    return item * index;
+});
+
+console.log(newArr);
+
+
+const sum = arr.reduce(function(total, next) {
+    return total + next;
+});
+
+console.log(sum);
+// --------------------------
+const filter = arr.filter(function(item) {
+    return item % 2 === 0;
+});
+
+console.log(filter);
+// --------------------------
+const find = arr.find(function(item) {
+    return item === 4;
+});
+
+console.log(find);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
 // ========= variáveis de Escopos ====================================
 function teste(x) {
     let y = 2;
@@ -14,19 +65,7 @@ teste(10);
 const usuario = {nome: 'João'};
 usuario.nome = 'Diego';
 console.log(usuario);
-
-
-
-
-
-
-
-
-
-
-
-
-
+ */
 
 // ============== CLASSES ==============================================
 /* Ele já vai mostrar a soma porque o método é static ele não vai precisar
