d26b2de5d540ca7289998ee9ce5de8f1cb4619f6
Jonatas Rosa Moura <jonatas.rmoura@outlook.com>
2020-07-27 22:58:34 -0300

    Conceitos de Classe ES5+
    Classe, Herança e métodos static

--- bundle.js
+++ bundle.js
@@ -6,18 +6,60 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-var Teste = /*#__PURE__*/function () {
-  function Teste() {
-    _classCallCheck(this, Teste);
+/* Ele já vai mostrar a soma porque o método é static ele não vai precisar
+definir uma nova classe Matematica e não depende de mais nenhuma informação
+da nossa classe */
+var Matematica = /*#__PURE__*/function () {
+  function Matematica() {
+    _classCallCheck(this, Matematica);
   }
 
-  _createClass(Teste, [{
-    key: "metodo",
-    value: function metodo() {}
-  }, {
-    key: "outroMetodo",
-    value: function outroMetodo() {}
+  _createClass(Matematica, null, [{
+    key: "soma",
+    value: function soma(a, b) {
+      return a + b;
+    }
   }]);
 
-  return Teste;
+  return Matematica;
 }();
+
+console.log(Matematica.soma(1, 2));
+console.log(Matematica.soma(100, 900));
+console.log(Matematica.soma(50, 50));
+console.log(Matematica.soma(6, 4));
+/* 
+// Herança JS
+class List {
+    constructor() {
+        this.data = [];
+    };
+
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
+    };
+};
+
+class TodoList extends List {
+    constructor() {
+        // chama o constructor da classe pai
+        super();
+
+        this.usuario = "Diego";
+    };
+
+    mostrarUsuario() {
+        console.log(this.usuario);
+    };
+};
+
+// Intanciar essa primeira classe
+var MinhaLista =  new TodoList();
+
+document.querySelector(`button#novotodo`).onclick = function() {
+    MinhaLista.add('Novo todo');
+};
+
+MinhaLista.mostrarUsuario();
+ */
--- main.js
+++ main.js
@@ -1,9 +1,67 @@
-class Teste {
-    metodo() {
+/* Ele já vai mostrar a soma porque o método é static ele não vai precisar
+definir uma nova classe Matematica e não depende de mais nenhuma informação
+da nossa classe */
 
-    }
-
-    outroMetodo() {
-
+class Matematica {
+    static soma(a, b) {
+        return a+b;
     }
 }
+
+console.log(Matematica.soma(1, 2));
+console.log(Matematica.soma(100, 900));
+console.log(Matematica.soma(50, 50));
+console.log(Matematica.soma(6, 4));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+// Herança JS
+class List {
+    constructor() {
+        this.data = [];
+    };
+
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
+    };
+};
+
+class TodoList extends List {
+    constructor() {
+        // chama o constructor da classe pai
+        super();
+
+        this.usuario = "Diego";
+    };
+
+    mostrarUsuario() {
+        console.log(this.usuario);
+    };
+};
+
+// Intanciar essa primeira classe
+var MinhaLista =  new TodoList();
+
+document.querySelector(`button#novotodo`).onclick = function() {
+    MinhaLista.add('Novo todo');
+};
+
+MinhaLista.mostrarUsuario();
+ */
